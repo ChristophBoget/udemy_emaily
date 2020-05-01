0d85bae6185ffee5632e0eb73bda326b334382c0
Christoph Boget <christoph+developer@project-mayhem.org>
2020-05-01 18:09:57 -0400

    Adding serialize/deserialize back in

--- services/passport.js
+++ services/passport.js
@@ -4,17 +4,20 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys.js');
 
 const User = mongoose.model('users');
-
+/*
 passport.serializeUser((user, done) => {
+console.log('serializeUser', user);
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
+console.log('deserializeUser', id, (typeof id));
   User.findById(id).then(user => {
+console.log('user', user);
     done(null, user);
   });
 });
-
+*/
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
